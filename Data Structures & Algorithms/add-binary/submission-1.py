864556487e84cc4e753d5cd436b13ba85bcb7720
Raul Flores <58998160+raulf21@users.noreply.github.com>
2026-08-28 10:53:09 -0700

    Add: add-binary - submission-1

---
+++ Data Structures & Algorithms/add-binary/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        result = []
+        carry = 0
+
+        # pointers that start end of strings
+        i = len(a) -1 
+        j = len(b) - 1
+
+        while i >= 0 or j >= 0 or carry:
+            digit_a = int(a[i]) if i>= 0 else 0
+            digit_b = int(b[j]) if j >= 0 else 0
+
+            total = digit_a + digit_b + carry
+
+            result.append(str(total % 2))
+
+            carry = total // 2
+
+            # move pointers
+            i -=1
+            j -=1
+        return ''.join(result[::-1])
+
+        
